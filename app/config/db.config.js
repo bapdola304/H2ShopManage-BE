fa6d883b5d97b8553a29f0b36fb8a22cd1f3f19b
Hung Ngo <bapdola304@gmail.com>
2023-01-01 17:07:03 +0700

    update mongoDB url

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://admin:CzSZssIAegX9I9SuIL1pLQY0@MongoS3601A.back4app.com:27017/b70464e3382a4b78b2350c61a7c967fe"
+  url: "mongodb+srv://admin:Ngohung0@cluster0.7enckdc.mongodb.net/?retryWrites=true&w=majority"
 };
